54afe62f6ddc173ad3d4979e87503a4df1104c2b
jescalaya <jescalaya@adr.com.pe>
2020-06-10 12:57:33 -0500

    Primer Commit

---
+++ pryRealizarBackup/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnOperacion = New System.Windows.Forms.Button()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDirectorio = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnOperacion)
+        Me.GroupBox1.Controls.Add(Me.txtNombre)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtDirectorio)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 7)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(352, 142)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'btnOperacion
+        '
+        Me.btnOperacion.Image = Global.pryRealizarBackup.My.Resources.Resources._48px_Crystal_Clear_action_db_update
+        Me.btnOperacion.Location = New System.Drawing.Point(245, 65)
+        Me.btnOperacion.Name = "btnOperacion"
+        Me.btnOperacion.Size = New System.Drawing.Size(91, 67)
+        Me.btnOperacion.TabIndex = 4
+        Me.btnOperacion.Text = "Crear Respaldo"
+        Me.btnOperacion.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        Me.btnOperacion.UseVisualStyleBackColor = True
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Enabled = False
+        Me.txtNombre.Location = New System.Drawing.Point(10, 89)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(218, 20)
+        Me.txtNombre.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nombre de Archivo"
+        '
+        'txtDirectorio
+        '
+        Me.txtDirectorio.Enabled = False
+        Me.txtDirectorio.Location = New System.Drawing.Point(10, 36)
+        Me.txtDirectorio.Name = "txtDirectorio"
+        Me.txtDirectorio.Size = New System.Drawing.Size(336, 20)
+        Me.txtDirectorio.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Directorio de Backup"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(375, 159)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Crear Backup"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnOperacion As System.Windows.Forms.Button
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtDirectorio As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ pryRealizarBackup/Form1.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim conex As New SqlConnection("DATA SOURCE = EQUIPO\MSSQL2; INITIAL CATALOG = dbGestionVentas; INTEGRATED SECURITY = true")
+    Dim cmd As SqlCommand
+    Dim fecha As String = Format(DateTime.Now, "_dd_MM_yyy_h-mm")
+    Dim archivo As String = "dbGestionVentas" & fecha & ".bak"
+    Dim ubicacion As String = "C:\Users\Usuario1\Documents\MEGA\"
+    Dim parametro As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        llenarText()
+    End Sub
+
+    Sub llenarText()
+        Me.txtNombre.Text = archivo
+        Me.txtDirectorio.Text = ubicacion
+    End Sub
+
+    Sub crearBackup()
+        conex.Close()
+        Try
+            parametro = ubicacion & archivo
+            cmd = New SqlCommand
+            With cmd
+                .Connection = conex
+                .CommandType = CommandType.StoredProcedure
+                .CommandText = "sp_CrearBackup"
+                .Parameters.AddWithValue("@Ubicacion", parametro)
+            End With
+
+            conex.Open()
+            cmd.ExecuteNonQuery()
+            conex.Close()
+            MsgBox("Backup creado correctamente!", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Operación exitosa!")
+        Catch ex As Exception
+            MsgBox("Error al crear Backup" & Chr(13) & ex.Message, MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "Error!")
+        End Try
+    End Sub
+
+    Private Sub btnOperacion_Click(sender As Object, e As EventArgs) Handles btnOperacion.Click
+        crearBackup()
+    End Sub
+
+End Class
